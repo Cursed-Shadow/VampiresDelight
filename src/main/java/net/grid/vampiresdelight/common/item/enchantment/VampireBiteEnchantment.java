d2df3f0e8edd5562a1a14f3b89c5b974d069c43b
GridExpert <grid.write.here@gmail.com>
2024-04-22 17:46:01 +0300

    Filter ru_RU

--- src/main/java/net/grid/vampiresdelight/common/item/enchantment/VampireBiteEnchantment.java
+++ src/main/java/net/grid/vampiresdelight/common/item/enchantment/VampireBiteEnchantment.java
@@ -1,6 +1,5 @@
 package net.grid.vampiresdelight.common.item.enchantment;
 
-import de.teamlapen.vampirism.items.VampirismVampireSwordItem;
 import net.grid.vampiresdelight.VampiresDelight;
 import net.grid.vampiresdelight.common.registry.VDEnchantments;
 import net.minecraft.util.RandomSource;
@@ -9,23 +8,21 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
 
 public class VampireBiteEnchantment extends Enchantment {
     public VampireBiteEnchantment(Rarity rarity) {
-        super(rarity, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+        super(rarity, VDEnchantments.VAMPIRE_SWORD, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
     }
 
     public static void healFromDamage(LivingEntity user, int level, float damage) {
         RandomSource randomSource = user.getRandom();
         if (randomSource.nextInt(5) < 3 && user instanceof Player player) {
-            player.heal((float) level / 20 * damage);
+            player.heal((float) level / 30 * damage);
         }
     }
 
@@ -57,11 +54,6 @@ public class VampireBiteEnchantment extends Enchantment {
     @Override
     public int getMaxCost(int enchantmentLevel) {
         return super.getMinCost(enchantmentLevel) + 50;
-    }
-
-    @Override
-    public boolean canApplyAtEnchantingTable(@NotNull ItemStack stack) {
-        return stack.getItem() instanceof VampirismVampireSwordItem;
     }
 
     @Override
--- src/main/java/net/grid/vampiresdelight/common/registry/VDEnchantments.java
+++ src/main/java/net/grid/vampiresdelight/common/registry/VDEnchantments.java
@@ -1,13 +1,18 @@
 package net.grid.vampiresdelight.common.registry;
 
+import de.teamlapen.vampirism.items.VampirismVampireSwordItem;
 import net.grid.vampiresdelight.VampiresDelight;
 import net.grid.vampiresdelight.common.item.enchantment.VampireBiteEnchantment;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class VDEnchantments {
+    public static final EnchantmentCategory VAMPIRE_SWORD = EnchantmentCategory.create("vampire_sword", item -> item instanceof VampirismVampireSwordItem || item instanceof AxeItem);
+
     public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, VampiresDelight.MODID);
 
     public static final RegistryObject<Enchantment> VAMPIRE_BITE = ENCHANTMENTS.register("vampire_bite",
